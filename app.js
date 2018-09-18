7c964e778cd193c715cd1a324ea87bb40883281a
Lab User <LabUser@labs.salina.k-state.edu>
2018-09-18 10:47:52 -0500

    Models

--- app.js
+++ app.js
@@ -9,6 +9,16 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+
+//Set up mongoose connection
+var mongoose = require('mongoose');
+var mongoDB = 'mongodb://jbaumchen:Baumchen12@ds261302.mlab.com:61302/local_library';
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
