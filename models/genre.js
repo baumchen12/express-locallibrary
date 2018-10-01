6169e93f6a75f3ea705b987249cb9916d53121bc
Lab User <LabUser@labs.salina.k-state.edu>
2018-10-01 14:26:52 -0500

    Subheading tutorials // Completed Web Pages

--- models/genre.js
+++ models/genre.js
@@ -2,11 +2,9 @@ var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema;
 
-var GenreSchema = new Schema(
-{
-	genre: { type: string, required: true, min:3, max:100 }, //reference to the associated genre
- }
-);
+var GenreSchema = new Schema({
+	name: {type: String, required: true, min:3, max:100} //reference to the associated genre
+ });
 
 // Virtual for genre's URL
 GenreSchema
